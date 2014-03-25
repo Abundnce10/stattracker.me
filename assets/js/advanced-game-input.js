7fe0b03466def5d226986087bdb25a3669567260
Abundnce10 <michaelstitt@gmail.com>
2014-03-24 22:03:38 -0700

    working on layout for advanced-game-input

---
+++ assets/js/advanced-game-input.js
@@ -0,0 +1,61 @@
+$(document).ready(function(e) {
+
+/*
+	var $court = $("#basketball-court-container");
+	var resizeTimer; // Set resizeTimer to empty so it resets on page load
+
+
+	function resizeFunction() {
+	    // Stuff that should happen on resize
+
+	    // grab width and height
+			var $courtWidth = $court.outerWidth();
+			var $courtHeight = $court.outerHeight();
+
+			console.log($courtWidth);
+			console.log($courtHeight);
+
+			drawCourt($court, $courtWidth, $courtHeight);
+	};
+
+	// On resize, run the function and reset the timeout
+	// 250 is the delay in milliseconds. Change as you see fit.
+	$(window).resize(function() {
+	    clearTimeout(resizeTimer);
+	    resizeTimer = setTimeout(resizeFunction, 250);
+	});
+
+	// on page load run function
+	resizeFunction();
+
+
+	function drawCourt(courtElem, courtWidth, courtHeight) {
+
+		// start building <svg> basketball court
+		html = '<svg id="basketball_court" xmlns="http://www.w3.org/2000/svg"';
+		html += ' version="1.1" width="'+courtWidth+'" height="'+courtHeight+'">';
+
+		// .... build out court
+
+
+		// close <svg>
+		html += '</svg>';
+		// append to court container
+		courtElem.append(html);
+
+
+	};// end drawCourt()
+*/
+
+
+	// 1. Get layout right at all size
+	// 2. Find width of basketball container, then determine height
+	// 3. Create lines/circles based on dimensions
+
+
+
+});
+
+
+
+
